9c008a9a763a3c7eab9feb471f3aa78034bd0cfb
Pavel Ilin <ilin.pa@gmail.com>
2012-12-17 17:10:19 +0100

    Driver is contiki process

--- projects/mrf24j40_driver/dev/mrf24j40.c
+++ projects/mrf24j40_driver/dev/mrf24j40.c
@@ -13,6 +13,9 @@ static int mrf24j40_pending_packet(void);
 static int mrf24j40_on(void);
 static int mrf24j40_off(void);
 
+PROCESS(mrf24j40_process, "MRF24J40 driver");
+
+
 const struct radio_driver mrf24j40_driver =
   {
     .init = mrf24j40_init,
@@ -75,4 +78,27 @@ static int mrf24j40_on(void)
 static int mrf24j40_off(void)
 {
 	return 0;
+}
+
+void mrf24j40_interrupt(void)
+{
+	process_poll(&mrf24j40_process);
+}
+
+ISR(MRF24J40_INTERRUPT)
+{
+	mrf24j40_interrupt();
+}
+
+
+PROCESS_THREAD(mrf24j40_process, ev, data)
+{
+	PROCESS_BEGIN();
+
+	while (1)
+	{
+		PROCESS_YIELD_UNTIL(PROCESS_EVENT_POLL == ev);
+	}
+
+	PROCESS_END();
 }
